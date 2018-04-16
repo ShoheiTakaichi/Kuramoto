663c686f2d6e9dd2017e04cdfa5f0fb7bb88f5b4
takaichi <shohei.takaichi@gmail.com>
2018-04-16 16:11:55 +0900

    all corect

--- kuramoto/generator/main.hs
+++ kuramoto/generator/main.hs
@@ -2,8 +2,8 @@ import Numeric.LinearAlgebra
 import Graphics.Gnuplot.Simple
 
 
-k = 2.0 :: R--結合強度
-dt = 0.01 :: R--結合強度
+k = 1.8 :: R--結合強度
+dt = 0.001 :: R--結合強度
 
 
 --order parameter の計算
@@ -24,11 +24,20 @@ make_data::(Vector R,Vector R) -> Int -> (Matrix R,Vector C)
 make_data (v,x_0) n = (fromRows (take n phi),psi)
   where
     phi = x_0 : map (f v) phi
-    psi = fromList ( map order (take n phi))
-    
+    psi = fromList (map order (take n phi))
+
+
+lorentz::Double -> Int -> [R]
+lorentz gamma n = [gamma * tan (pi*x) | p <- [1..n],let x = (toEnum p)/(toEnum n+1)-0.5 :: R]
+  
 
 main = do
-  let x = fromList [0,1,0]::Vector R
-  let v = fromList [-1,0,1]::Vector R
-  let (a,b) = make_data (v,x) 10:: (Matrix R,Vector C)
-  print a
+  let n = 1000 :: Int
+  let v = fromList (lorentz 2.0 n)::Vector R
+  let x = fromList (replicate n 0)::Vector R
+  let (a,b) = make_data (v,x) 10000:: (Matrix R,Vector C)
+  let b_r = map realPart (toList b)
+  let b_c = map imagPart (toList b)
+  print (order x)
+  plotList [PNG "test.png"] b_r
+
