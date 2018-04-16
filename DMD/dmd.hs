7d3b85486ff3710510ce97b9a45cbf29c1376156
takaichi <shohei.takaichi@gmail.com>
2018-04-16 19:37:03 +0900

    update DMD -compactSVD- #2

--- DMD/dmd.hs
+++ DMD/dmd.hs
@@ -8,8 +8,9 @@ dmd :: Matrix C -> IO ((Vector C, Matrix C))
 dmd v = do
   let v1 = subMatrix (0,0) ((fst$size v),(snd$size v)-1) v
   let v2 = subMatrix (0,1) ((fst$size v),(snd$size v)-1) v
-  let (u,sigma,w) = svd v1
-  let s = (tr u) <> v2 <> w <> (complex (diagRect 0 (1/sigma) (fst$size w) (fst$size u)))::Matrix C
+  let (u,sigma,w) = compactSVDTol 0.1 v1 --特異値の打ち切り値、多分
+  let s = (tr u) <> v2 <> w <> (complex (diagRect 0 (1/sigma) (snd$size w) (snd$size u)))::Matrix C
+  print s
   let (labmda,phi) = eig s :: (Vector C,Matrix C)
   return (labmda, u <> phi)
 
@@ -18,11 +19,11 @@ initial_energy v phi = (pinv (tr phi)) #> v
 
 
 
---{-
+{-
 check :: Matrix C
 check = fromColumns x
   where
-    lam = diag (fromList [0.8,0.9,0.95])
+    lam = diag (fromList [0.0,0.0,0.95])
     v_1 = fromList [1,0,0]::Vector C
     v_2 = fromList [0,1,0]::Vector C
     v_3 = fromList [0,0,1]::Vector C
@@ -33,6 +34,5 @@ main = do
   (a,b) <- dmd (check)
   saveMatrix "real.txt" "%lf" (fst$fromComplex check)
   saveMatrix "complex.txt" "%lf" (snd$fromComplex check)
-  print (initial_energy  (check #> fromList ([1]++replicate (snd (size check)-1) 0)) b)
-  print b
---}
+  --print (initial_energy  (check #> fromList ([1]++replicate (snd (size check)-1) 0)) b)
+}
