50a95ffcf6a356eb23b1aaf195f0863b431c1475
takaichi <shohei.takaichi@gmail.com>
2018-05-09 18:42:54 +0900

    resume err

--- modules/singleAnalysis.hs
+++ modules/singleAnalysis.hs
@@ -8,13 +8,13 @@ import CustomPlot
 import Control.Parallel.Strategies
 
 main = do
-  let n = 1000 :: Int
+  let n = 5000 :: Int
   let v = fromList (lorentz 1.0 n)::Vector R
   --let x = fromList (linearScale (toInteger n-1) (-pi,pi)) :: Vector R
   let x = fromList (replicate n 0)::Vector R
   let (k,dt)=(1.60,0.00050)
-  let (a,b) = parMakeData (k,dt) (v,x) 5000:: (Matrix R,Vector C)
-  let rho = phaseDensity 200 a
+  let (a,b) = parMakeData (k,dt) (v,x) 10000:: (Matrix R,Vector C)
+  let rho = phaseDensity 250 a
   let (lam,mode) = (dmd ( toComplex (rho,rho*0)))::(Vector C,Matrix C)
   --let energy = (fst . fromComplex) (initialEnergy x mode)
   --saveMatrix "energy.txt" "%lf" (fromColumns [energy])
@@ -22,8 +22,12 @@ main = do
   plotOrder b dt
   --plotList [PNG "test.png"] (toList (fst (fromComplex lam)))
   print$size lam
-  print (log ((toList (fst(fromComplex lam)))!!0) / dt)
+  --print (log ((toList (fst(fromComplex lam)))!!0) / dt)
   g (fst (fromComplex mode)) (size lam-1)
+  print$norm_1 (toColumns rho)
+
+  
+
 
 
 --f = map ((take 100) . fourier) . toColumns
